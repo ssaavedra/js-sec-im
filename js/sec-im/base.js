005d1ddb0536930e6b361bed1df730c5d2a32959
Santiago Saavedra <santiagosaavedra@gmail.com>
2011-11-21 16:41:13 +0100

    Dojified JSBN.

---
+++ js/sec-im/base.js
@@ -0,0 +1,5 @@
+if(typeof dojo == "undefined")
+	throw "I can't load elements without Dojo!";
+
+dojo.provide("sec-im.base");
+dojo.require("sec-im.rsa");
---
+++ js/sec-im/rsa.js
@@ -0,0 +1,2 @@
+dojo.provide("sec-im.rsa");
+
